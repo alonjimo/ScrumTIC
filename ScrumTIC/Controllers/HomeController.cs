b8e91facdbbf55f761b185dbcca9eed5cbeac073
alonjimo <alonjimo@gmail.com>
2014-08-28 18:59:46 -0600

    Cambio de titulo de Prueba

--- ScrumTIC/Controllers/HomeController.cs
+++ ScrumTIC/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ScrumTIC.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Hola :) Cambios en Github";
 
             return View();
         }
